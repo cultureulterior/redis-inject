7d495aaa827697fdd3c0d89f8b20fe55b112ae91
Samuel Kleiner <sk@pusher.com>
2013-11-12 11:28:12 +0000

    Clean up shell script

--- redis_murder.sh
+++ redis_murder.sh
@@ -1,11 +1,24 @@
+#!/bin/bash
+if [[ -z $(pgrep -f redis-server) ]]
+then
+    echo "### No redis server"
+    exit -1
+fi
+
 export DATE=$(date "+%s")
+export VERSION=$(redis-server -v | cut -f 4 -d " " )
+
+echo "### Retrieving redis '${VERSION}'"
 git clone git@github.com:antirez/redis.git
 pushd redis
-git checkout $(redis-server -v | cut -f 4 -d " " )
+git checkout $VERSION
 popd
-echo "Getting Address"
-export ADDRESS=$( printf "p &server \n detach" | gdb -q --batch --pid=`pgrep -f redis-server` 2>&1 | tee -a redis_murder.log | grep -F "data variable" | cut -f 2 -d ')' | tr -d ' ' )
-echo "Address: '$ADDRESS'"
+
+echo "### Getting Address"
+export ADDRESS=$( printf "p &server \n detach" | gdb -q --pid=`pgrep -f redis-server` 2>&1 | tee -a redis_murder.log | grep -F "data variable" | cut -f 3 -d ')' | tr -d ' ' )
+echo "*Address: '$ADDRESS'"
+
+echo "### Compiling Injection 'redis_murder_$DATE'"
 if [[ $(uname) -eq  "Darwin" ]]
    then
    ## Using options from https://github.com/scummvm/scummvm/blob/master/configure
@@ -13,7 +26,6 @@ if [[ $(uname) -eq  "Darwin" ]]
 else
    gcc -Wall -DADDRESS=$ADDRESS -I$PWD/redis/src -Wl,--just-symbols,$(which redis-server) -c redis_murder.c -o redis_murder_$DATE
 fi
-echo "Injecting Code"
-printf "p (void *) dlopen(\"$PWD/redis_murder_$DATE\",2) \n detach" | gdb -q --batch --command=redis_murder.gdb_tmp --pid=`pgrep -f redis-server` 2>&1 >> redis_murder.log
-echo $DATE
-redis-server -v
+
+echo "### Injecting Code"
+printf "p (void *) dlopen(\"$PWD/redis_murder_$DATE\",2) \n detach" | gdb -q --pid=`pgrep -f redis-server` 2>&1 | tee -a redis_murder.log > /dev/null
